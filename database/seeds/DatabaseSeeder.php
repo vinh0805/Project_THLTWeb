dd9d6b6d9b0d98b4890ad45273994bb9cde36d0b
phongdk29101999 <phongdk@tmh-techlab.vn>
2020-12-16 10:59:19 +0700

    add column email to table users

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
