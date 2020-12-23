f374819493cc36c8d18ff185677fc00eda700b35
phongdk29101999 <phong.dk176843@sis.hust.edu.vn>
2020-12-23 10:03:27 +0700

    add funtion notifications details

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Notification;
+use App\Models\Post;
 
 class NotificationController extends Controller
 {
@@ -14,6 +15,17 @@ class NotificationController extends Controller
 
     public function show($id) {
         $notification = Notification::find($id);
-        return $notification;
+        $postId = $notification->post_id;
+        $post = Post::find($postId);
+        if(!isset($post)){
+            echo "Have bug!!!";
+        } else {
+            $allComments = DB::table('comments')->join('users', 'comments.user_id', '=', 'users.id')
+                ->select('comments.*', 'users.*')->where('post_id', '=', $post->id)->get();
+            // $allComments = Comment::where('post_id', '=', $post->id)->get();
+            return view('posts.screen13-show-post')
+                ->with('post', $post)->with('allComments', $allComments);
+        }
+        return 0;
     }
 }
