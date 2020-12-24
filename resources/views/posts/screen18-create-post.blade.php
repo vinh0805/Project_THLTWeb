641d0325ddbd2f7969107b2a7c74ede25d05d836
balduu <ducboom30@gmail.com>
2020-12-24 20:36:13 +0700

    postcreateUI

--- resources/views/posts/screen18-create-post.blade.php
+++ resources/views/posts/screen18-create-post.blade.php
@@ -1,32 +1,36 @@
 @extends('layout')
 @section('content')
-    <h1>Create a new post</h1>
+    <h1 class = "PostCreateTitle">Create a new post</h1>
     <form action="{{url('post/save')}}" method="post">
         @csrf
-        <div>
-            <label for="postTitle">Title</label>
-            <input id="postTitle" name="postTitle" placeholder="...">
+        <div class = "post-option">
+            <div class = "option">
+                <label for="postTitle">Title</label>
+                <input id="postTitle" class="input-form" name="postTitle" placeholder="...">
+            </div>
+            <div class = "option">
+                <label for="postCategoryPet">Pet</label>
+                <select id="postCategoryPet" class="input-form" name="postCategoryPet">
+                    @foreach($allCategoryPet as $categoryPet)
+                        <option value="{{$categoryPet->id}}">{{$categoryPet->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class = "option">
+                <label for="postCategory">Category</label>
+                <select id="postCategory" class="input-form" name="postCategory">
+                    @foreach($allCategory as $category)
+                        <option value="{{$category->id}}">{{$category->name}}</option>
+                    @endforeach
+                </select>
+            </div>
         </div>
-        <div>
-            <label for="postCategoryPet">Pet</label>
-            <select id="postCategoryPet" name="postCategoryPet">
-                @foreach($allCategoryPet as $categoryPet)
-                    <option value="{{$categoryPet->id}}">{{$categoryPet->name}}</option>
-                @endforeach
-            </select>
-        </div>
-        <div>
-            <label for="postCategory">Category</label>
-            <select id="postCategory" name="postCategory">
-                @foreach($allCategory as $category)
-                    <option value="{{$category->id}}">{{$category->name}}</option>
-                @endforeach
-            </select>
+        <div class = "post-submit">
+            <button type="submit" id="createPostSubmitButton" class="post-button">Create</button>
         </div>
         <div>
             <label for="postContent">Content</label>
-            <textarea id="postContent" name="postContent"></textarea>
+            <textarea id="postContent" name="postContent" class = "post-content"></textarea>
         </div>
-        <button type="submit" id="createPostSubmitButton">Create</button>
     </form>
 @endsection
