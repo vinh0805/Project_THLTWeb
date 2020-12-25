5d282c94ebdde739bc21648d670599cf40ef9d92
Mau Tien Vinh <vinhemt@gmail.com>
2020-12-25 16:36:45 +0700

    fix show post view

--- resources/views/posts/screen13-show-post.blade.php
+++ resources/views/posts/screen13-show-post.blade.php
@@ -5,21 +5,25 @@
         <div id="showPostContent">{!! $post->content !!}</div>
     </section>
 
-    <section id="showComment" @if($post->status == 0) hidden @endif>
-        <h2><b>Comment</b></h2>
-        @foreach($allComments as $comment)
-            <div class="userComment"><b>{{$comment->name}}</b>: </div>
-            <div>{{$comment->content}}</div>
-        @endforeach
-    </section>
-
-    <section id="writeComment" @if($post->status || 0 && !isset($user)) hidden @endif>
-        <form action="{{url('post/' . $post->id . '/write-comment')}}" method="post">
-            @csrf
-            <label>
-                <textarea id="commentContent" name="commentContent" placeholder="Write comment here..."></textarea>
-            </label>
-            <button type="submit" id="writeCommentButton">Write comment</button>
-        </form>
-    </section>
+    @if($post->status == 1)
+        <section id="showComment">
+            <h2><b>Comment</b></h2>
+            @foreach($allComments as $comment)
+                <div class="userComment"><b>{{$comment->name}}</b>: </div>
+                <div>{{$comment->content}}</div>
+            @endforeach
+        </section>
+        <?php $user = \Illuminate\Support\Facades\Session::get('sUser')?>
+        @if(isset($user))
+            <section id="writeComment">
+                <form action="{{url('post/' . $post->id . '/write-comment')}}" method="post">
+                    @csrf
+                    <label>
+                        <textarea id="commentContent" name="commentContent" placeholder="Write comment here..."></textarea>
+                    </label>
+                    <button type="submit" id="writeCommentButton">Write comment</button>
+                </form>
+            </section>
+        @endif
+    @endif
 @endsection
