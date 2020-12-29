c69c73716f5c458f25263ce14e324dc61c2d25b5
balduu <ducboom30@gmail.com>
2020-12-29 15:51:32 +0700

    Notification_byChung

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -6,18 +6,20 @@
         <h1>List Notifications</h1>
     @if (count($notifications) > 0)
         @foreach ($notifications as $notification)
-        <div class="notification" id="<?= $notification->status == 0 ? '': 'seen' ?>">
+        <div class="new_notification" id="<?= $notification->status == 0 ? '': 'seen' ?>">
             <a href="{{url('/me/notifications/' . $notification->id)}}">
                 <div>
                     <div class="comment-text">
                         <span class="username">
+                            <div class = "request_user">
                             <span class="text-muted float-right" id="noti-time">{{$notification->created_at}}</span>
                             <span class="user float-left" id="noti-from">
-                                <img class="img-circle img-sm" src="{{url('frontend/images/Logo.png')}}" alt="User Image">
-                                From:
+                            From:
+                                <img class="request_user_avt" src="{{url('frontend/images/Logo.png')}}" alt="User Image">
                             </span>
                             <span class="user float-left" id=noti-name>{{$notification->name}}</span>
-                            <br>
+                            <br><hr>
+                            </div>
                             <span class="reply" id="noti-title">Re: {{$notification->title}}</span>
                         </span><!-- /.username -->
                         <p id=noti-content>{{$notification->content}}</p>
