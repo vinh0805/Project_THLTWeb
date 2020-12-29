6f1a9d6ebc7ed3ec050c1146d553b94780ff7f50
Mau Tien Vinh <vinhemt@gmail.com>
2020-12-29 17:39:49 +0700

    Fix at screen08

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\CategoryPet;
+use App\Models\Comment;
+use App\Models\LikePost;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
@@ -13,12 +16,25 @@ class CategoryController extends Controller
     {
         $categoryPet = CategoryPet::where('name', $categoryPetName)->first();
         $category = Category::where('name', $categoryName)->first();
+        $likePostArray = [];
+        $commentPostArray = [];
+        $i = 0;
         if(isset($categoryPet) && isset($category)){
-            $allPosts = Post::where('category_pet_id', $categoryPet->id)
-                ->where('category_id', $category->id)->where('status', 1)->get();
+            $allPosts = DB::table('posts')
+                ->join('users', 'posts.user_id', '=', 'users.id')
+                ->select('posts.*', 'users.name', 'users.avatar')
+                ->where('category_pet_id', $categoryPet->id)
+                ->where('category_id', $category->id)
+                ->where('status', 1)->get();
+            foreach ($allPosts as $post) {
+                $likePostArray[$i] = count(LikePost::where('post_id', '=', $post->id)->get());
+                $commentPostArray[$i] = count(Comment::where('post_id', '=', $post->id)->get());
+                $i++;
+            }
             return view('categories.screen08-pet-category')
                 ->with('allPosts', $allPosts)->with('categoryPet', $categoryPet)
-                ->with('category', $category);
+                ->with('category', $category)->with('likePostArray', $likePostArray)
+                ->with('commentPostArray', $commentPostArray);
         }
         return 0;
     }
--- resources/views/categories/screen08-pet-category.blade.php
+++ resources/views/categories/screen08-pet-category.blade.php
@@ -7,12 +7,13 @@
         echo '<span id="loginError">' . $message . '</span>';
         Session::put('message', null);
     }
-    $user = \Illuminate\Support\Facades\Session::get('sUser')
+    $user = \Illuminate\Support\Facades\Session::get('sUser');
+    $i = 0;
     ?>
 
     <section class = "category-body">
         <h1>{{$categoryPet->name}} {{$category->name}}</h1>
-        <div class = "category-list"> 
+        <div class = "category-list">
             <hr>
             @foreach($allPosts as $post)
                 <div class = "category-post">
@@ -21,7 +22,13 @@
                         <p>{!! $post->content !!} </p>
                     </div>
                     <hr>
+                    <div class="content-foot">
+                        <div>{{$likePostArray[$i]}}  <i class="fas fa-paw"></i></div>
+                        <div>{{$commentPostArray[$i]}}  <i class="fas fa-comment-dots"></i></div>
+                        <div class="content-foot-user">Posted by <a href="{{url('user/' . $post->user_id . '/info')}}">{{$post->name}}</a> at {{$post->created_at}}</div>
+                    </div>
                 </div>
+                <?php $i++ ?>
             @endforeach
         </div>
     </section>
